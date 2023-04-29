c930a8eb82be0e9db9dc59c73287dc775575b97f
x1337x1 <hachemimohamed99@gmail.com>
2023-04-29 14:53:00 +0100

    version four

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
